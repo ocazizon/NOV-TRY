0c6b84f72c87a61b4f4e48061261264911be3e4e
ocazizon <65603020+ocazizon@users.noreply.github.com>
2024-04-23 10:17:26 -0400

    0423

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log("fakja;sdfj")
-prompt("asdfasdf")
+document.querySelector('.two').addEventListener('click', ()=> {
+    document.querySelector('.three').innerHTML =
+     document.querySelector('.one').value
+    console.log("sadfdddd")
+})
+
+console.log(sdfjl)
